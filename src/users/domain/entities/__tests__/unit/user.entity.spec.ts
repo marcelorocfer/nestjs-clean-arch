98561d6291061980fa1edb349924e4c87de47b98
marcelo <marceloengecomp@gmail.com>
2023-10-20 12:19:09 -0300

    Update methods - unit tests

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -1,7 +1,7 @@
 import { UserEntity, UserProps } from '../../user.entity';
 import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder';
 
-describe("UserEntity unit tests", () => {
+describe('UserEntity unit tests', () => {
   let props: UserProps;
   let sut: UserEntity;
 
@@ -10,7 +10,7 @@ describe("UserEntity unit tests", () => {
     sut = new UserEntity(props);
   });
 
-  it("Constructor method", () => {
+  it('Constructor method', () => {
     expect(sut.props.name).toEqual(props.name);
     expect(sut.props.email).toEqual(props.email);
     expect(sut.props.password).toEqual(props.password);
@@ -20,6 +20,12 @@ describe("UserEntity unit tests", () => {
   it('Getter of name field', () => {
     expect(sut.props.name).toBeDefined();
     expect(sut.props.name).toEqual(props.name);
+    expect(typeof sut.props.name).toBe('string');
+  });
+
+  it('Setter of name field', () => {
+    sut['name'] = 'other name';
+    expect(sut.props.name).toEqual('other name');
     expect(typeof sut.props.name).toBe('string');
   });
 
@@ -35,8 +41,19 @@ describe("UserEntity unit tests", () => {
     expect(typeof sut.props.password).toBe('string');
   });
 
+  it('Setter of password field', () => {
+    sut['password'] = 'other password';
+    expect(sut.props.password).toEqual('other password');
+    expect(typeof sut.props.password).toBe('string');
+  });
+
   it('Getter of createdAt field', () => {
     expect(sut.props.createdAt).toBeDefined();
     expect(sut.props.createdAt).toBeInstanceOf(Date);
+  });
+
+  it('Should update the password field', () => {
+    sut.updatePassword('other password');
+    expect(sut.props.password).toEqual('other password');
   });
 });
