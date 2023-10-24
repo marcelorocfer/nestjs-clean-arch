068aa3078a89728a1d497cd0ba06dd7229ccce12
marcelo <marceloengecomp@gmail.com>
2023-10-24 11:33:37 -0300

    Refactoring UserEntity unit tests

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -6,11 +6,13 @@ describe('UserEntity unit tests', () => {
   let sut: UserEntity;
 
   beforeEach(() => {
+    UserEntity.validate = jest.fn();
     props = UserDataBuilder({});
     sut = new UserEntity(props);
   });
 
   it('Constructor method', () => {
+    expect(UserEntity.validate).toHaveBeenCalled();
     expect(sut.props.name).toEqual(props.name);
     expect(sut.props.email).toEqual(props.email);
     expect(sut.props.password).toEqual(props.password);
@@ -52,7 +54,14 @@ describe('UserEntity unit tests', () => {
     expect(sut.props.createdAt).toBeInstanceOf(Date);
   });
 
+  it('Should update a user', () => {
+    expect(UserEntity.validate).toHaveBeenCalled();
+    sut.update('other name');
+    expect(sut.props.name).toEqual('other name');
+  });
+
   it('Should update the password field', () => {
+    expect(UserEntity.validate).toHaveBeenCalled();
     sut.updatePassword('other password');
     expect(sut.props.password).toEqual('other password');
   });
