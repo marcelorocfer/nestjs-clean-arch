25b73012e1a02e4e93aeaf02460ebf0f58531b35
marcelo <marceloengecomp@gmail.com>
2023-10-25 11:27:02 -0300

    Integration tests - valid fields on create user

--- src/users/domain/entities/__tests__/integration/user.entity.int.spec.ts
+++ src/users/domain/entities/__tests__/integration/user.entity.int.spec.ts
@@ -96,5 +96,14 @@ describe('UserEntity integration tests', () => {
       }
       expect(() => new UserEntity(props)).toThrowError(EntityValidationError);
     });
+
+    it('Should a valid user', async () => {
+      expect.assertions(0);
+
+      const props: UserProps = {
+        ...UserDataBuilder({}),
+      }
+      new UserEntity(props);
+    });
   });
 });
