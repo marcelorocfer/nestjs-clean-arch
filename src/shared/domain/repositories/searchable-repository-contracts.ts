37ad5ae4433fd1e62aeeca1be124d53f1108e06d
marcelo <marceloengecomp@gmail.com>
2023-11-06 12:44:17 -0300

    Unit tests SearchParams class

--- src/shared/domain/repositories/searchable-repository-contracts.ts
+++ src/shared/domain/repositories/searchable-repository-contracts.ts
@@ -18,12 +18,12 @@ export class SearchParams {
   protected _sortDir: SortDirection | null;
   protected _filter: string | null;
 
-  constructor(props: SearchProps) {
-    this._page = props.page;
-    this._perPage = props.perPage;
-    this._sort = props.sort;
-    this._sortDir = props.sortDir;
-    this._filter = props.filter;
+  constructor(props: SearchProps = {}) {
+    this.page = props.page;
+    this.perPage = props.perPage;
+    this.sort = props.sort;
+    this.sortDir = props.sortDir;
+    this.filter = props.filter;
   }
 
   get page() {
@@ -43,8 +43,9 @@ export class SearchParams {
   }
 
   private set perPage(value: number) {
-    let _perPage = +value;
-    if (Number.isNaN(_perPage) || _perPage <= 0 || parseInt(_perPage as any) !== _perPage) {
+    let _perPage = value === (true as any) ? this._perPage : +value;
+    if (
+      Number.isNaN(_perPage) || _perPage <= 0 || parseInt(_perPage as any) !== _perPage) {
       _perPage = this._perPage;
     }
     this._perPage = _perPage;
@@ -62,9 +63,9 @@ export class SearchParams {
     return this._sortDir;
   }
 
-  private set sortDir(value: SortDirection | null) {
+  private set sortDir(value: string | null) {
     if (!this.sort) {
-      this.sortDir = null;
+      this._sortDir = null;
       return;
     }
     const dir = `${value}`.toLowerCase();
