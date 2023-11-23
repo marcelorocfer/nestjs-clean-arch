cd8e600749d3ebe100fa748c5bd9333b214f26f6
marcelo <marceloengecomp@gmail.com>
2023-11-23 12:07:09 -0300

    Implementing ListUsersUseCase

--- src/users/application/usecases/list-users.usecase.ts
+++ src/users/application/usecases/list-users.usecase.ts
@@ -1,12 +1,13 @@
 import { UserRepository } from "@/users/domain/repositories/user.repository";
-import { UserOutput } from "../dto/user-output";
 import { UseCase as DefaultUseCase } from "@/shared/application/usecases/use-case";
 import { SearchInput } from "@/shared/application/dto/search-input";
+import { PaginationOutput, PaginationOutputMapper } from "@/shared/application/dto/pagination-output";
+import { UserOutput, UserOutputMapper } from "../dto/user-output";
 
 export namespace ListUsersUseCase {
   export type Input = SearchInput;
 
-  export type Output = UserOutput;
+  export type Output = PaginationOutput<UserOutput>;
 
   export class UseCase implements DefaultUseCase<Input, Output> {
     constructor(
@@ -16,7 +17,14 @@ export namespace ListUsersUseCase {
     async execute(input: Input): Promise<Output> {
       const params = new UserRepository.SearchParams(input);
       const searchResult = await this.userRepository.search(params);
-      return;
+      return this.toOutput(searchResult);
+    }
+
+    private toOutput(searchResult: UserRepository.SearchResult): Output {
+      const items = searchResult.items.map(item => {
+        return UserOutputMapper.toOutput(item)
+      });
+      return PaginationOutputMapper.toOutput(items, searchResult);
     }
   }
 }
