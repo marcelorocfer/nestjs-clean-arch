10a6e56d13b4799184728671a8ff9e40097d381c
marcelo <marceloengecomp@gmail.com>
2023-12-19 11:56:03 -0300

    Configuring dtos in nestjs

---
+++ src/users/infrastructure/dto/update-password.dto.ts
@@ -0,0 +1,6 @@
+import { UpdatePasswordUseCase } from "@/users/application/usecases/update-password.usecase";
+
+export class UpdatePasswordDto implements Omit<UpdatePasswordUseCase.Input, 'id'> {
+  password: string;
+  oldPassword: string;
+}
