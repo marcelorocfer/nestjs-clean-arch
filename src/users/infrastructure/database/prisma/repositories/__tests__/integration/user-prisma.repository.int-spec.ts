7ae85a82b1d0b3080c04825ad8a4b9efe9529929
marcelo <marceloengecomp@gmail.com>
2024-02-14 16:53:38 -0300

    Organize imports

--- src/users/infrastructure/database/prisma/repositories/__tests__/integration/user-prisma.repository.int-spec.ts
+++ src/users/infrastructure/database/prisma/repositories/__tests__/integration/user-prisma.repository.int-spec.ts
@@ -1,12 +1,11 @@
-import { PrismaClient } from "@prisma/client";
-import { UserPrismaRepository } from "../../user-prisma.repository";
-import { Test, TestingModule } from "@nestjs/testing";
-import { setupPrismaTests } from "@/shared/infrastructure/database/prisma/testing/setup-prisma-tests";
-import { DatabaseModule } from "@/shared/infrastructure/database/database.module";
-import { rejects } from "assert";
 import { NotFoundError } from "@/shared/domain/errors/not-found-error";
+import { DatabaseModule } from "@/shared/infrastructure/database/database.module";
+import { setupPrismaTests } from "@/shared/infrastructure/database/prisma/testing/setup-prisma-tests";
 import { UserEntity } from "@/users/domain/entities/user.entity";
 import { UserDataBuilder } from "@/users/domain/testing/helpers/user-data-builder";
+import { Test, TestingModule } from "@nestjs/testing";
+import { PrismaClient } from "@prisma/client";
+import { UserPrismaRepository } from "../../user-prisma.repository";
 
 describe('UserPrismaRepository integration tests', () => {
   const prismaService = new PrismaClient();
