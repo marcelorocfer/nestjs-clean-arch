9a912d14cb788d8ac19463103f4563093d66cec6
marcelo <marceloengecomp@gmail.com>
2024-09-03 16:08:03 -0300

    Refactor UsersController e2e tests

--- src/users/infrastructure/__tests__/e2e/create.e2e-spec.ts
+++ src/users/infrastructure/__tests__/e2e/create.e2e-spec.ts
@@ -50,16 +50,11 @@ describe('UsersController unit tests', () => {
         .send(signupDto)
         .expect(201)
 
-      expect(Object.keys(res.body)).toStrictEqual([
-        'id',
-        'name',
-        'email',
-        'createdAt'
-      ]);
-      const user = await repository.findById(res.body.id);
+      expect(Object.keys(res.body)).toStrictEqual(['data']);
+      const user = await repository.findById(res.body.data.id);
       const presenter = UsersController.userToResponse(user.toJSON());
       const serialized = instanceToPlain(presenter);
-      expect(res.body).toStrictEqual(serialized);
+      expect(res.body.data).toStrictEqual(serialized);
     });
   });
 
