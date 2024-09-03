2fabe60afdb3dd693f9e3ce8290df00ab1bc18c2
marcelo <marceloengecomp@gmail.com>
2024-09-03 16:23:52 -0300

    Config ValidationPipe and DTOs

--- src/users/infrastructure/dto/update-password.dto.ts
+++ src/users/infrastructure/dto/update-password.dto.ts
@@ -1,6 +1,12 @@
 import { UpdatePasswordUseCase } from "@/users/application/usecases/update-password.usecase";
+import { IsString, IsNotEmpty } from "class-validator";
 
 export class UpdatePasswordDto implements Omit<UpdatePasswordUseCase.Input, 'id'> {
+  @IsString()
+  @IsNotEmpty()
   password: string;
+
+  @IsString()
+  @IsNotEmpty()
   oldPassword: string;
 }
